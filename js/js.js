d9a9f971d8bad80412288e2105257fc5fac4ab1d
wawatai <wataiPublic@gmail.com>
2022-11-18 14:36:59 +0800

    111-11-18

--- js/js.js
+++ js/js.js
@@ -1,4 +1,15 @@
 $(function(){
+    //自動調整視窗高
+    $(function(){
+        var th = $('.mainwrap');
+        var wh = window.innerHeight;
+        th.css("height", (String(wh - 202) + "px"));
+        $(window).resize(function () {
+            var th = $('.mainwrap');
+            var wh = window.innerHeight;
+            th.css("height", (String(wh - 202) + "px"));
+        });
+    });
     //會員資料
     $("header .member .user").click(function(){
         $("header .member .dropList").toggleClass("display");
@@ -20,5 +31,22 @@ $(function(){
     //點擊收起
     $(".gameBlock .title").click(function(){
         $(this).parent().toggleClass("active");
+    })
+
+    //開啟聊天室
+    $(".chatroom .titleArea").click(function(){
+        $(".chatroom").toggleClass("active");
+    })
+
+    //傳送訊息
+    $(".chatroom textarea").keydown(function(event){
+
+        if ( event.which == 13)
+        {
+            var text = $("textarea").val();
+            $(".chatroom .text").append("<li><p>"+ text +"</p></li>");
+
+            $(this).val(" ");
+        }
     })
 })
